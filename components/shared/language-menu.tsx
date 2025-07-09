8e8bc04d282d03abfb97421705a00f705885a1ce
shamsiKing <shamsiking00@gmail.com>
2025-07-09 18:52:28 +0500

    endi dropdownlngda tilarni almashtisa boladi

--- components/shared/language-menu.tsx
+++ components/shared/language-menu.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from "react";
 import {
   DropdownMenu,
@@ -10,8 +12,14 @@ import { Button } from "../ui/button";
 import { Languages } from "lucide-react";
 import { lngs } from "@/constants";
 import Image from "next/image";
+import Link from "next/link";
+import { useParams } from "next/navigation";
+import { cn } from "@/lib/utils";
 
 const LanguageMenu = () => {
+  const { lng } = useParams();
+  console.log(lng);
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
@@ -22,17 +30,19 @@ const LanguageMenu = () => {
       <DropdownMenuContent>
         <DropdownMenuGroup>
           {lngs.map((item) => (
-            <DropdownMenuItem key={item.route} className="cursor-pointer">
-              <Image
-                src={`/assets/${item.route}.jpg`}
-                alt={item.label}
-                width={30}
-                height={30}
-              />
-              <span className="ml-2 font-spaceGrotesk-mono font-medium">
-                {item.label}
-              </span>
-            </DropdownMenuItem>
+            <Link href={item.route} key={item.route}>
+              <DropdownMenuItem className={cn("cursor-pointer")}>
+                <Image
+                  src={`/assets/${item.route}.jpg`}
+                  alt={item.label}
+                  width={30}
+                  height={30}
+                />
+                <span className="ml-2 font-spaceGrotesk-mono font-medium">
+                  {item.label}
+                </span>
+              </DropdownMenuItem>
+            </Link>
           ))}
         </DropdownMenuGroup>
       </DropdownMenuContent>
